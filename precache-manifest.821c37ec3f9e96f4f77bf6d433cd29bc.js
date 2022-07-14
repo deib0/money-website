82284aaa7ea29224ec725b34b91d693675d32f62
deibo <deib0@foxmail.com>
2022-07-14 18:33:35 +0800

    deploy

---
+++ precache-manifest.821c37ec3f9e96f4f77bf6d433cd29bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbdbdbc8572d12c1f1d",
+    "url": "/money-website/css/app.08d1314f.css"
+  },
+  {
+    "revision": "2792c809c500565412d99b5d5cb39330",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "dfbdbdbc8572d12c1f1d",
+    "url": "/money-website/js/app.3d76ebf4.js"
+  },
+  {
+    "revision": "0534a0f46a702d01f292",
+    "url": "/money-website/js/chunk-vendors.152e72f8.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/money-website/precache-manifest.821c37ec3f9e96f4f77bf6d433cd29bc.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "money"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
